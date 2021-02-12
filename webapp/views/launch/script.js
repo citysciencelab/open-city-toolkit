6b63186c44ecd700977a43abff31573106313d19
TTPL-0164 <blessyferns2597@gmail.com>
2021-02-12 13:11:58 +0530

    cancel button functionality

--- webapp/views/launch/script.js
+++ webapp/views/launch/script.js
@@ -640,7 +640,14 @@ function launchSettings(value) {
 }
 
 function reply(res, message) {
-  if(res.id == 'buffer_module.2'){
+  if(res.id == 'buffer_module.1'){
+    const res = {
+      id : 'buffer_module.4',
+      message : 'Process Cancelled.'
+    }
+    handleResponse(res)
+  }
+  else if(res.id == 'buffer_module.2'){
     const res = {
       id : 'buffer_module.3',
       message : 'Select Layers'
